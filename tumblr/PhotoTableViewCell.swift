d5d6d495c3da3940011538c18a5d7d57b19b13c2
Rageeb Mahtab <>
2018-09-12 20:52:59 -0400

    Final Commit

---
+++ tumblr/PhotoTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoTableViewCell.swift
+//  tumblr
+//
+//  Created by Rageeb Mahtab on 9/12/18.
+//  Copyright © 2018 Rageeb Mahtab. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoTableViewCell") as! PhotoTableViewCell
+        
+        // Configure YourCustomCell using the outlets that you've defined.
+        
+        return cell
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ tumblr/PhotosViewController.swift
@@ -0,0 +1,111 @@
+//
+//  PhotosViewController.swift
+//  tumblr
+//
+//  Created by Rageeb Mahtab on 9/12/18.
+//  Copyright © 2018 Rageeb Mahtab. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoTableViewCell", for: indexPath) as! PhotoTableViewCell
+        let post = posts[indexPath.row]
+        
+        if let photos = post["photos"] as? [[String: Any]] {
+            // photos is NOT nil, we can use it!
+            // TODO: Get the photo url
+            // 1.
+            let photo = photos[0]
+            // 2.
+            let originalSize = photo["original_size"] as! [String: Any]
+            // 3.
+            let urlString = originalSize["url"] as! String
+            // 4.
+            let url = URL(string: urlString)
+            cell.photoImageView.af_setImage(withURL: url!)
+        }
+        
+        return cell
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        
+        tableView.insertSubview(refreshControl, at: 0)
+        
+        fetchAllPhotos()
+        
+       
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func fetchAllPhotos() {
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                
+                // TODO: Get the posts and store in posts property
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                // TODO: Reload the table view
+                self.tableView.reloadData()
+                // Stops refreshControl from spinning 1 second after data returns
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
+                    self.refreshControl.endRefreshing()
+                })
+            }
+        }
+        task.resume()
+    }
+    
+    @objc func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        fetchAllPhotos()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
